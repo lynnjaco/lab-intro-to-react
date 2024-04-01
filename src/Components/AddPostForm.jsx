7b8a1d298af0650fe91574a059c53b85479b3c7a
lynnjaco <jacobs.lynnette@gmail.com>
2024-04-01 17:30:12 -0400

    adds css styling to add post form component

--- src/Components/AddPostForm.jsx
+++ src/Components/AddPostForm.jsx
@@ -1,11 +1,13 @@
+import "./AddPostForm.css"
+
 export const AddPostForm = () => {
     return (
-        <div>
+        <div id="add-post-form-container">
             <form>
                 <h4>Add Post</h4>
                 <label>Post Title<input type="text"></input></label>
-                <label>Post<input type="text" placeholder="Write your post here..."></input></label>
-                <label>Post Image<input type="file"></input></label>
+                <label><input type="file"></input></label>
+                <label><input id="post-text-entry-box" type="text" placeholder="Write your post here..."></input></label>
                 <button type="submit">Add Post</button>
             </form>
         </div>
